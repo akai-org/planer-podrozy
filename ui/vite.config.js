062c16ba9c5a46a2150929a27cb43afacd151b52
Marcin <marcinkrueger@onet.eu>
2022-12-28 19:15:10 +0100

    Simultaneous mobile and desktop check

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -3,12 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    	port: 8080,
-	host: true,
-	watch: {
-      	usePolling: true
-	}
-  },
+	plugins: [react()],
+	server: {
+		port: 8080,
+		host: true,
+		watch: {
+			usePolling: true,
+		},
+	},
+	preview: {
+		port: 8090,
+	},
 });
