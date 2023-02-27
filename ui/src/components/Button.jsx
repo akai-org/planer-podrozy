77300188a4268d9554f88e3ef85b85d0c6f8f75b
Alan Grądecki <alangradecki@wp.pl>
2023-02-27 16:39:15 +0100

    updated naming, props and styles

--- ui/src/components/Button.jsx
+++ ui/src/components/Button.jsx
@@ -1,18 +1,22 @@
 import PropTypes from 'prop-types'
-import classes from './Button.module.css'
+import classes from './Button.module.scss'
 
 const Button = ({ variant, onClick, children }) => {
   return (
-    <button className={classes[`variant-${variant}`]} onClick={onClick}>
+    <button className={classes[`${variant}`]} onClick={onClick}>
       {children}
     </button>
   )
 }
 
 Button.propTypes = {
-  variant: PropTypes.number.isRequired,
+  variant: PropTypes.number,
   onClick: PropTypes.func.isRequired,
-  children: PropTypes.any.isRequired,
+  children: PropTypes.string.isRequired,
+}
+
+Button.defaultProps = {
+  variant: 'blue',
 }
 
 export default Button
