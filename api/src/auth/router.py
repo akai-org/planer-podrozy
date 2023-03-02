de180b9dfdc9458399da2068bba4fc095e5e2685
Adam Krawczyk <adam.ja.krawczyk@student.put.poznan.pl>
2023-03-02 20:47:17 +0100

    Black fix

--- api/src/auth/router.py
+++ api/src/auth/router.py
@@ -10,5 +10,4 @@ router = APIRouter(prefix="/api", tags=["auth"])
     "/registration", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut
 )
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-
     return {"message": "Hello"}
