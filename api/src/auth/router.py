31cb70681d68b5417d42cc37721e3a833a592fa4
Piotr <piotr.las@protonmail.com>
2023-03-08 09:05:50 +0100

    Update registration endpoint

--- api/src/auth/router.py
+++ api/src/auth/router.py
@@ -1,4 +1,4 @@
-from auth import schemas
+from auth import models, schemas
 from database import get_db
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
@@ -10,4 +10,14 @@ router = APIRouter(prefix="/api", tags=["auth"])
     "/registration", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut
 )
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    return {"message": "Hello"}
+    db_user = db.query(models.email).filter(models.User.email == user.email).first()
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered"
+        )
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    # db.refresh(new_user)
+
+    return {"message": "created"}
