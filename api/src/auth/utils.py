94145f91f73d30a945670a03ba995493f4ff0ead
Maksymilian Pływaczyk <plywak12331@gmail.com>
2023-04-05 21:14:57 +0200

    Fix name of 'AlGORITHM', fix names in schemas.py with password and hashed_password

--- api/src/auth/utils.py
+++ api/src/auth/utils.py
@@ -7,7 +7,7 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 ACCESS_TOKEN_EXPIRE_TIME = 30
-ALGORITH = "HS256"
+ALGORITHM = "HS256"
 
 
 def get_hashed_password(password: str):
@@ -25,13 +25,13 @@ def create_access_token(subject: Union[str, any], expires_delta: int = None) -> 
         expires_delta = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_TIME)
 
     to_encode = {"exp": expires_delta, "subject": str(subject)}
-    encoded_jwt = jwt.encode(to_encode, key=SECRET, algorithm=ALGORITH)
+    encoded_jwt = jwt.encode(to_encode, key=SECRET, algorithm=ALGORITHM)
     return encoded_jwt
 
 
 def verify_reset_password_token(token: str) -> Optional[str]:
     try:
-        decoded_token = jwt.decode(token, key=SECRET, algorithms=[ALGORITH])
+        decoded_token = jwt.decode(token, key=SECRET, algorithms=[ALGORITHM])
         return decoded_token["subject"]
     except jwt.InvalidTokenError:
         return None
