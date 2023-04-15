531ec9a85633dd8854cbc93bbf517d2a311df129
Alan Grądecki <alangradecki@wp.pl>
2023-04-16 00:51:49 +0200

    updated Button component

--- ui/src/components/Button/Button.jsx
+++ ui/src/components/Button/Button.jsx
@@ -2,6 +2,27 @@ import PropTypes from 'prop-types'
 import classes from './Button.module.scss'
 import classNames from 'classnames'
 
+/**
+ * ### example: <br/>
+ * ```
+ * import Button from './components/Button/Button'
+ *
+ *<Button
+ *  variant="purple"
+ *  onClick={() => 'Clicked!'}
+ *  style='css_style'>
+ *    Button
+ *</Button>
+ *
+ *
+ * ```
+ * ### props: <br/>
+ * - variant - color variant<br/>colors available: ["blue", "dark-blue", "purple", "light-red", "black", "white"]<br/>isRequired: false<br /> default value: "blue"
+ * - onClick - event that will be triggered when the button is clicked<br/>isRequired: true<br /> default value: " "
+ * - children - content that will be displayed in the button<br/> isRequired: true<br />default value: " "
+ * - style - additional css styles<br/>isRequired: false<br /> default value: " "
+ */
+
 const Button = ({ variant, onClick, children, style }) => {
   return (
     <button className={classNames(classes[variant], style)} onClick={onClick}>
@@ -11,14 +32,18 @@ const Button = ({ variant, onClick, children, style }) => {
 }
 
 Button.propTypes = {
-  variant: PropTypes.number,
+  /** color variant */
+  variant: PropTypes.string,
+  /** event that will be triggered when the button is clicked */
   onClick: PropTypes.func.isRequired,
+  /** content that will be displayed in the button */
   children: PropTypes.string.isRequired,
-  style: PropTypes.string
+  /** additional css styles */
+  style: PropTypes.string,
 }
 
 Button.defaultProps = {
-  variant: 'blue'
+  variant: 'blue',
 }
 
 export default Button
