9f8127fdf30071252dea9833cc761a4fb0fc20e6
Bambo <kyrstke@gmail.com>
2023-04-21 14:18:57 +0200

    Changed /registration route to /register

--- api/src/auth/router.py
+++ api/src/auth/router.py
@@ -10,7 +10,7 @@ router = APIRouter(prefix="/api", tags=["auth"])
 
 
 @router.post(
-    "/registration", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut
+    "/register", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut
 )
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = get_user_by_email(db=db, user_email=user.email)
