2275ffd16d2d1ce1d7812b51c349a0901c75ace8
Bambo <kyrstke@gmail.com>
2023-05-23 15:46:35 +0200

    Deleted TODO comment

--- api/src/auth/router/__init__.py
+++ api/src/auth/router/__init__.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter
 
-from auth.schemas import UserCreate, UserOut, UserRead
+from auth.schemas import UserCreate, UserOut, UserRead, UserUpdate
 from auth.utils import auth_backend, fastapi_users
 
 router = APIRouter(prefix="/api", tags=["auth"])
 
 router.include_router(fastapi_users.get_register_router(UserOut, UserCreate))
-# TODO: change requires verification to True when verification added
 router.include_router(
     fastapi_users.get_auth_router(auth_backend, requires_verification=True)
 )
