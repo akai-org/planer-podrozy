f5e195b53ad28bc7e16860bb7bb25e9345871650
Arashayo <djdominko@gmail.com>
2023-07-27 13:48:56 +0200

    syntax fix

--- ui/src/components/TextLink/TextLink.jsx
+++ ui/src/components/TextLink/TextLink.jsx
@@ -1,33 +1,34 @@
-import React from 'react';
-import './TextLink.scss';
-import { FiExternalLink } from 'react-icons/fi';
-import PropTypes from 'prop-types';
+import React from 'react'
+import './TextLink.scss'
+import { FiExternalLink } from 'react-icons/fi'
+import PropTypes from 'prop-types'
 
 const TextLink = ({ url, text, external }) => {
   const handleClick = () => {
-    external ? window.open(url, '_blank') : window.location.href = url;
-  };
+    external ? window.open(url, '_blank') : window.location.href = url
+  }
 
   return (
     <a
       href={url}
       target={external ? '_blank' : '_self'}
+      rel={external ? 'noopener noreferrer' : ''}
       onClick={handleClick}
-      className={`text-link ${external ? 'external' :''}`}
+      className={`text-link ${external ? 'external' : ''}`}
     >
       {text}
       {external && <FiExternalLink className='external-icon'/>}
     </a>
-  );
-};
+  )
+}
 
 TextLink.propTypes = {
-    /** destination URL that the link you want to navigate to*/
-    url: PropTypes.string.isRequired,
-    /** content that will be displayed in the link */
-    text: PropTypes.string.isRequired,
-    /** boolean that indicates whether the link is external or internal  */
-    external: PropTypes.bool,
-};
+  /** destination URL that the link you want to navigate to */
+  url: PropTypes.string.isRequired,
+  /** content that will be displayed in the link */
+  text: PropTypes.string.isRequired,
+  /** boolean that indicates whether the link is external or internal  */
+  external: PropTypes.bool,
+}
 
-export default TextLink;
+export default TextLink
