494dc506c7db3bcbff8e15c3cf1ec1a7990bf0d5
Kacper Nadstoga <kapix007@gmail.com>
2023-08-05 13:29:54 +0200

    add comments to variable file and create short docummentation for FormInput component

--- ui/src/components/FormInput/FormInput.jsx
+++ ui/src/components/FormInput/FormInput.jsx
@@ -1,8 +1,35 @@
 import PropTypes from 'prop-types';
 import styles from './FormInput.module.scss';
-import classNames from 'classnames'; 
+import classNames from 'classnames';
 
-const FormInput = ({id, labelName, inputType, placeholder, required, className, darkTheme}) => {
+/**
+ * ### example: <br/>
+ * ```
+ * import FormInput from './components/FormInput/FormInput'
+ *
+ *<FormInput
+ *  id="email"
+ *  labelName="E-mail"
+ *  inputType="email"
+ *  placeholder="xyz@gmail.com"
+ *  required
+ *  className
+ *  />
+ *    
+ *
+ *
+ *
+ * ```
+ * ### props: <br/>
+ * - id - identificator of input element - it will connect with label to improve **a11y**<br/> isRequired: true<br/>default value: "basicInput"
+ * - labelName - value for label to improve readable input<br/> isRequired: true<br/>default value: "Label"
+ * - input type - type of your input: email, password, text and range etc.<br/> isRequired: true<br/>default value: "text"
+ * - placeholder for input (recommend for text inputs)<br/> isRequired: true<br/>default value: "placholder"
+ * - required - set if user must fill this input or it's optional<br/> isRequired: true<br/>default value: true
+ * - darkTheme - additional modificatior for class to change some rules for dark theme - local implementation<br/> isRequired: true<br/>default value: false
+ */
+
+const FormInput = ({id, labelName, inputType, placeholder, required, darkTheme}) => {
     const themePicker = darkTheme ? 'inputFragment--dark' : 'inputFragment';
 
     const placeholderText = inputType !== 'password' ? placeholder : null;
@@ -44,8 +71,8 @@ FormInput.defaultProps = {
     id:"basicInput",
     labelName: 'Label',
     inputType: 'text',
-    placeholder: 'xyz',
-    required: false,
+    placeholder: 'placeholder',
+    required: true,
     darkTheme: false,
 }
 
